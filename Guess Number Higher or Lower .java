5fd1668611791a82d994b13d54e2bb53e327a26c
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-11-10 12:10:16 +0530

    week 12

---
+++ Guess Number Higher or Lower .java
@@ -0,0 +1,18 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1, right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result > 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
